925074ece846dcd6141b1959e7802cbbaf851e24
miora-ando <miandofa@gmail.com>
2026-04-13 15:04:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+/* SCROLL REVEAL */
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+  reveals.forEach(el => {
+    const top = el.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if(top < windowHeight - 100){
+      el.classList.add("active");
+    }
+  });
+});
+
+/* NEWSLETTER DEMO */
+document.getElementById("newsletter-form").addEventListener("submit",function(e){
+  e.preventDefault();
+  alert("Merci pour votre inscription 💌");
+});
